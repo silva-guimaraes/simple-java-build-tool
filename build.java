0a173cc3823eb0155450a3ae8ae85e4a0bce66a6
silva guimaraes <silva__guimaraes@hotmail.com>
2023-10-09 23:12:46 -0300

    documentação++++

--- build.java
+++ build.java
@@ -42,7 +42,6 @@ class Dependency {
 
 class build 
 {
-    Path pwd;
 
     public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
         File destFile = new File(destinationDir, zipEntry.getName());
@@ -124,14 +123,14 @@ class build
         }
 
         var target = new File(args[0]);
-        // var parent = target.getParent() == null ? "." : target.getParent();
-        // var workingDir = Paths.get(parent);
 
         if (!target.exists()) {
             System.err.println(target + " file does not exist.");
             System.exit(1);
         }
 
+        // nessa ferramenta dependencias sao declaradas com comentarios no inicio
+        // do arquivo principal e baixadas e instaladas automaticamente.
         var dependencies = new ArrayList<Dependency>();
         try {
             var filereader = new FileReader(target);
@@ -167,21 +166,24 @@ class build
                     // if (path.toFile().exists())
                     //     continue;
 
+                    // baixar dependencias
+                    System.out.println("downloading dependency: " + dependency.url);
                     var request = HttpRequest.newBuilder()
                         .uri(URI.create(dependency.url))
                         .build();
-                    System.out.println("downloading dependency: " + dependency.url);
-
                     client.send(request, BodyHandlers.ofFile(path));
 
                     dependency.path = path;
 
+                    // remover do zip para que arquivos .class possam ser adicionados
+                    // ao .jar final
                     unzipFile(path.toString(), buildDir.toString());
                 }
             } catch (Exception e) {
                 e.printStackTrace();
                 System.exit(1);
             }
+            // adicionar bibliotecas ao classpath para a compilação
             buildArgs.add("-cp");
             buildArgs.add(
                     dependencies.stream()
@@ -194,16 +196,18 @@ class build
         buildArgs.add("-d");
         buildArgs.add(buildDir.toString());
 
+        // compilar javac
         runCommand(buildArgs.toArray(new String[0]), Paths.get("."), true);
 
         var mainClass = target.getName().split("\\.")[0];
 
         // cria um MANIFEST temporário para ser adicionado ao .jar
+        final String manifestName =  "___manifest___";
         final var manifest = 
             "Manifest-Version: 1.0\nClass-Path: .\nMain-Class: " + mainClass + "\n";
 
-        var manifestPath = Paths.get(buildDir.toString(), "___manifest___");
-        manifestPath.toFile().deleteOnExit();
+        var manifestPath = Paths.get(buildDir.toString(), manifestName);
+        // manifestPath.toFile().deleteOnExit();
         try { 
             FileOutputStream outputStream = 
                 new FileOutputStream(manifestPath.toFile());
@@ -214,36 +218,16 @@ class build
             System.exit(1);
         }
 
-        // try {
-        //     if (dependencies.size() > 0) {
-        //         // mover todos os arquivos .class para a pasta build para que
-        //         // possam ser incluidos dentro do jar junto com todas as dependencias
-        //         Files.list(workingDir)
-        //             .filter(x -> x.toString().endsWith(".class"))
-        //             .forEach(x -> moveFile(x, buildDir));
-        //         // incluir todos os arquivos dentro da pasta build 
-        //         // (nosso programa e dependencias)
-        //         jarFiles.add(buildDir.toString());
-        //     } else {
-        //         // adicionar apenas arquivos .class ao jar
-        //         jarFiles.addAll(Files.list(workingDir)
-        //                 .map(Path :: toString)
-        //                 .filter(x -> x.endsWith(".class"))
-        //                 .collect(Collectors.toList())
-        //                 );
-        //     }
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        //     System.exit(1);
-        // }
-
+        // esse comando é executado dentro do diretório build
         ArrayList<String> jarArgs = new ArrayList<String>();
         jarArgs.add("jar");
         jarArgs.add("cvfm");
+        // criar .jar fora diretório build
         jarArgs.add(Paths.get("..", mainClass + ".jar").toString());
-        jarArgs.add("___manifest___");
+        jarArgs.add(manifestName);
         jarArgs.add(".");
 
+        // criar .jar
         runCommand(jarArgs.toArray(new String[0]), buildDir, false);
     }
 }
--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@
 
 rm -f test/*.class test/*.jar test/MANIFEST.MF &&
     rm -rf test/build &&
-    # export PWD=$(realpath test) &&
     cp build.jar test/. &&
     cd test &&
     java -jar build.jar Test.java &&
